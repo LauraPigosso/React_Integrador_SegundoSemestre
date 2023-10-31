253f5fa4809766ee6f44dfbd43238e3f49f6665f
Laura Pigosso <127753547+LauraPigosso@users.noreply.github.com>
2023-10-31 16:52:21 -0300

    Add files via upload

--- src/assets/Layout/Header/Header.jsx
+++ src/assets/Layout/Header/Header.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import style from "./HeaderCss.css"
 import LogoTitulo from "../../static/Iamgens/LogoTitulo.png";
 import BotaoRosaPrincipal from "../../components/PinkButton";
+import './HeaderCss.css'; // Importe o arquivo CSS
 
 const Header = (props) => {
   return (
-    <>
     <div className="container">
       <div className="Header">
-        <img className="LogoTitulo" src={props.LogoTitulo} alt="" />
+        <img className="LogoTitulo" src={LogoTitulo} alt="" />
         <ul className="Lista">
-            <li>Sobre nós</li>
-            <li>Para Você</li>
+          <li>Sobre nós</li>
         </ul>
-        <ul><li className="BotaoRosa">Entrar</li></ul>
-        <BotaoRosaPrincipal frase="Crie uma conta"/>
+        <ul className="Lista">
+          <li>Para você</li>
+        </ul>
+        <div className="Botoes">
+          <ul>
+            <li className="BotaoRosa">Entrar</li>
+          </ul>
+          <BotaoRosaPrincipal frase="Abrir conta" />
+        </div>
       </div>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
